d9cef711b1a8f1774509c70abbe90aaf01f3b37e
chasem <cpmcquown@gmail.com>
2021-09-07 16:08:16 -0700

    adds intial code to user and thought models

--- models/Thought.js
+++ models/Thought.js
@@ -3,10 +3,14 @@ const { Schema, model } = require('mongoose');
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
-
+            type: String,
+            required: true,
+            // character limit should go below
         },
         createdAt: {
-
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
         },
         username: {
 
--- models/User.js
+++ models/User.js
@@ -3,10 +3,16 @@ const { Schema, model } = require('mongoose');
 const UserSchema = new Schema(
     {
         username: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
         email: {
-
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
         },
         thoughts: [],
         friends: []
